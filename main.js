9cbdd8fb57d6fe9d079d6fa7c0ec4950dfd98a28
Yirty2 <ketuanakakuteiyattaze@gmail.com>
2025-10-04 23:45:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function sendMail(body){
     params.append("text", body);
     params.append("subject", "websiteからの自動送信");
     params.append("mail", "yirty2@gmail.com");
-    fetch("https://script.google.com/macros/s/AKfycbz6MjB_8evRiJij1UMvdt9Lr3EPaI0bqsM722S1dtg3tBdI-CT275RlUJ742uJupRSNLw/exec", {method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:params});
+    fetch("https://script.google.com/macros/s/AKfycbyF-SFr4a7XJq7_nMUo5hGR7_6jUj_MfpPBYvUzYUtGHvstIcZ68GOhaHPgMccSo93xdA/exec", {method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:params});
 
     const btn=document.getElementById("btn");
     btn.innerHTML="送信済み";
